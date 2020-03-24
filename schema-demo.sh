c5f10079c42078840e0235747a1c536b16b5644f
JT Smith <12450077+SmithJohnTaylor@users.noreply.github.com>
2020-03-24 12:23:32 -0400

    Modifying for local

--- schema-demo.sh
+++ schema-demo.sh
@@ -4,13 +4,19 @@
 # include the magic
 ########################
 . demo-magic.sh -d
+
 #TYPE_SPEED=60
 DEMO_PROMPT="${GREEN}➜ ${CYAN}\W "
 
 clear
 
+########################
+# end demo magic config
+########################
+
 TOPIC=$1
-KAFKA_URL=
+KAFKA_URL=localhost:9092
+SR_URL=http://localhost:8081
 
 p "TopicName"
 echo $TOPIC
@@ -19,27 +25,27 @@ echo $TOPIC
 p ""
 p "Create topic "
 wait
-pe "kafka-topics --bootstrap-server localhost:9092 --create --topic $TOPIC --partitions 1 --replication-factor 3"
+pe "kafka-topics --bootstrap-server $KAFKA_URL --create --topic $TOPIC --partitions 1 --replication-factor 1"
 
 # Listing topics
 p ""
 p "List Topics"
 wait
-pe "kafka-topics --bootstrap-server localhost:9092 --list"
+pe "kafka-topics --bootstrap-server $KAFKA_URL --list"
 
 
 # List schemas
 p ""
 p "List all Subjects and Schemas"
 wait
-pe "curl --silent --basic --user $SR_AUTH -X GET https:///subjects | jq ."
+pe "curl --silent --basic -X GET $SR_URL/subjects | jq ."
 wait
 
 # Compatibility
 p ""
 p "Listing global schema compatibility level"
 wait
-pe "curl --silent --basic --user $SR_AUTH -X GET https:///config | jq ."
+pe "curl --silent --basic -X GET $SR_URL/config | jq ."
 wait
 
 # Register manually
@@ -67,38 +73,38 @@ wait
 ## Register it
 p ""
 p "Manual register the schema"
-p "curl --user SR_AUTH -X POST -H \"Content-Type: application/vnd.schemaregistry.v1+json\" --data '{\"schema\": \"{\"type\":\"record\",\"name\":\"Payment\",\"namespace\":\"io.confluent.examples.clients.basicavro\",\"fields\":[{\"name\":\"id\",\"type\":\"string\"},{\"name\":\"amount\",\"type\":\"double\"}]}\"}' https:///subjects/$TOPIC-value/versions"
+p "curl -X POST -H \"Content-Type: application/vnd.schemaregistry.v1+json\" --data '{\"schema\": \"{\"type\":\"record\",\"name\":\"Payment\",\"namespace\":\"io.confluent.examples.clients.basicavro\",\"fields\":[{\"name\":\"id\",\"type\":\"string\"},{\"name\":\"amount\",\"type\":\"double\"}]}\"}' $SR_URL/subjects/$TOPIC-value/versions"
 wait
-curl --user $SR_AUTH -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" --data '{"schema": "{\"type\":\"record\",\"name\":\"Payment\",\"namespace\":\"io.confluent.examples.clients.basicavro\",\"fields\":[{\"name\":\"id\",\"type\":\"string\"},{\"name\":\"amount\",\"type\":\"double\"}]}"}' https:///subjects/$TOPIC-value/versions | jq .
+curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" --data '{"schema": "{\"type\":\"record\",\"name\":\"Payment\",\"namespace\":\"io.confluent.examples.clients.basicavro\",\"fields\":[{\"name\":\"id\",\"type\":\"string\"},{\"name\":\"amount\",\"type\":\"double\"}]}"}' $SR_URL/subjects/$TOPIC-value/versions | jq .
 wait
 
 # Listing schema versions on our topic
 p ""
 p "List the number of versions"
-p "curl --silent --basic --user SR_AUTH -X GET https:///subjects/$TOPIC-value/versions | jq ."
+p "curl --silent --basic -X GET $SR_URL/subjects/$TOPIC-value/versions | jq ."
 wait
-curl --silent --basic --user $SR_AUTH -X GET https:///subjects/$TOPIC-value/versions | jq .
+curl --silent --basic -X GET $SR_URL/subjects/$TOPIC-value/versions | jq .
 wait
 # Display latest schema
 p ""
 p "Display latest schema"
-p "curl --silent --basic --user SR_AUTH -X GET https:///subjects/$TOPIC-value/versions/latest/schema | jq ."
+p "curl --silent --basic -X GET $SR_URL/subjects/$TOPIC-value/versions/latest/schema | jq ."
 wait
-curl --silent --basic --user $SR_AUTH -X GET https:///subjects/$TOPIC-value/versions/latest/schema | jq .
+curl --silent --basic -X GET $SR_URL/subjects/$TOPIC-value/versions/latest/schema | jq .
 wait
 # Compatibility topic
 p ""
 p "Display the topic compatibility"
-p "curl --silent --basic --user SR_AUTH -X GET -H "Content-Type: application/vnd.schemaregistry.v1+json" https:///config/$TOPIC-value?defaultToGlobal=true | jq ."
+p "curl --silent --basic -X GET -H \"Content-Type: application/vnd.schemaregistry.v1+json\" $SR_URL/config/$TOPIC-value?defaultToGlobal=true | jq ."
 wait
-curl --silent --basic --user $SR_AUTH -X GET -H "Content-Type: application/vnd.schemaregistry.v1+json" https:///config/$TOPIC-value?defaultToGlobal=true | jq .
+curl --silent --basic -X GET -H "Content-Type: application/vnd.schemaregistry.v1+json" $SR_URL/config/$TOPIC-value?defaultToGlobal=true | jq .
 wait
 # Change Compatibility
 p ""
 p "Change the compatibility to NONE"
-p "curl --silent --basic --user SR_AUTH -X PUT -H "Content-Type: application/vnd.schemaregistry.v1+json" --data '{"compatibility":"NONE"}' https:///config/$TOPIC-value | jq ."
+p "curl --silent --basic -X PUT -H \"Content-Type: application/vnd.schemaregistry.v1+json\" --data '{\"compatibility\":\"NONE\"}' $SR_URL/config/$TOPIC-value | jq ."
 wait
-curl --silent --basic --user $SR_AUTH -X PUT -H "Content-Type: application/vnd.schemaregistry.v1+json" --data '{"compatibility":"NONE"}' https:///config/$TOPIC-value | jq .
+curl --silent --basic -X PUT -H "Content-Type: application/vnd.schemaregistry.v1+json" --data '{"compatibility":"NONE"}' $SR_URL/config/$TOPIC-value | jq .
 wait
 
 ## Produce using current schema
@@ -106,15 +112,13 @@ p ""
 p "Produce messages with the schema"
 wait
 pe "kafka-avro-console-producer \
-    --broker-list localhost:9092 \
-    --property schema.registry.url=https:// \
-    --property schema.registry.basic.auth.user.info= \
-    --property basic.auth.credentials.source=USER_INFO \
+    --broker-list $KAFKA_URL \
+    --property schema.registry.url=$SR_URL \
     --topic $TOPIC \
     --property value.schema='{\"type\": \"record\",\"name\": \"Payment\",\"namespace\": \"io.confluent.examples.clients.basicavro\",\"fields\": [{\"name\": \"id\",\"type\": \"string\"},{\"name\": \"amount\",\"type\": \"double\"}]}'"
 # kafka-avro-console-producer \
-#     --broker-list localhost:9092 \
-#     --property schema.registry.url=https:// \
+#     --broker-list $KAFKA_URL \
+#     --property schema.registry.url=$SR_URL \
 #     --property schema.registry.basic.auth.user.info= \
 #     --property basic.auth.credentials.source=USER_INFO \
 #     --topic $TOPIC \
@@ -158,22 +162,19 @@ wait
 
 #
 pe "kafka-avro-console-producer \
-    --broker-list localhost:9092 \
-    --producer.config .ccloud/config \
-    --property schema.registry.url=https:// \
-    --property schema.registry.basic.auth.user.info= \
-    --property basic.auth.credentials.source=USER_INFO \
+    --broker-list $KAFKA_URL \
+    --property schema.registry.url=$SR_URL \
     --topic $TOPIC \
     --property value.schema='{\"type\": \"record\",\"name\": \"Payment\",\"namespace\": \"io.confluent.examples.clients.basicavro\",\"fields\": [{\"name\": \"id\",\"type\": \"string\"},{\"name\": \"amount\",\"type\": \"double\"},{\"name\": \"newfield\", \"type\": \"string\"}]}'"
 
-kafka-avro-console-producer \
-    --broker-list localhost:9092 \
-    --producer.config .ccloud/config \
-    --property schema.registry.url=https:// \
-    --property schema.registry.basic.auth.user.info= \
-    --property basic.auth.credentials.source=USER_INFO \
-    --topic $TOPIC \
-    --property value.schema='{"type": "record","name": "Payment","namespace": "io.confluent.examples.clients.basicavro","fields": [{"name": "id","type": "string"},{"name": "amount","type": "double"},{"name": "newfield", "type": "string"}]}'
+# kafka-avro-console-producer \
+#     --broker-list $KAFKA_URL \
+#     --producer.config .ccloud/config \
+#     --property schema.registry.url=$SR_URL \
+#     --property schema.registry.basic.auth.user.info= \
+#     --property basic.auth.credentials.source=USER_INFO \
+#     --topic $TOPIC \
+#     --property value.schema='{"type": "record","name": "Payment","namespace": "io.confluent.examples.clients.basicavro","fields": [{"name": "id","type": "string"},{"name": "amount","type": "double"},{"name": "newfield", "type": "string"}]}'
 # {"id": "111","amount": 10,"newfield": "sample123"}
 # {"id": "22","amount": 10,"newfield": {"string":"sample"}}
 # {"id": "33","amount": 10,"newfield": {"string":"sample"}}
@@ -189,50 +190,49 @@ p ""
 p "Consume the messages"
 wait
 pe "kafka-avro-console-consumer \
-    --bootstrap-server localhost:9092 \
-    --consumer.config .ccloud/config \
-    --property schema.registry.url=https:// \
-    --property schema.registry.basic.auth.user.info= \
-    --property basic.auth.credentials.source=USER_INFO \
+    --bootstrap-server $KAFKA_URL \
+    --property schema.registry.url=$SR_URL \
     --topic $TOPIC \
     --from-beginning"
 
 wait
 p "Fin"
 
-kafka-console-producer \
-    --broker-list localhost:9092 \
-    --producer.config .ccloud/config \
-    --topic $TOPIC
+# kafka-console-producer \
+#     --broker-list $KAFKA_URL \
+#     --producer.config .ccloud/config \
+#     --topic $TOPIC
 #
 # kafka-avro-console-consumer \
-#     --bootstrap-server localhost:9092 \
+#     --bootstrap-server $KAFKA_URL \
 #     --consumer.config .ccloud/config \
-#     --property schema.registry.url=https:// \
+#     --property schema.registry.url=$SR_URL \
 #     --property schema.registry.basic.auth.user.info= \
 #     --property basic.auth.credentials.source=USER_INFO \
 #     --topic $TOPIC \
 #     --from-beginning
 #
-kafka-console-consumer \
-    --bootstrap-server localhost:9092 \
-    --consumer.config .ccloud/config \
-    --topic $TOPIC \
-    --from-beginning
+# kafka-console-consumer \
+#     --bootstrap-server $KAFKA_URL \
+#     --consumer.config .ccloud/config \
+#     --topic $TOPIC \
+#     --from-beginning
 #
 # # Useful links
 # ## https://docs.confluent.io/current/schema-registry/index.html
 # ## https://docs.confluent.io/current/schema-registry/avro.html
 
 # # Delete
-# curl -X DELETE -u $SR_AUTH https:///subjects/
+# curl -X DELETE -u $SR_AUTH $SR_URL/subjects/
 
 # # Schema with backwards compatibility
 # kafka-avro-console-producer \
-#     --broker-list localhost:9092 \
+#     --broker-list $KAFKA_URL \
 #     --producer.config .ccloud/config \
-#     --property schema.registry.url=https:// \
+#     --property schema.registry.url=$SR_URL \
 #     --property schema.registry.basic.auth.user.info= \
 #     --property basic.auth.credentials.source=USER_INFO \
 #     --topic $TOPIC \
 #     --property value.schema='{"type": "record","name": "Payment","namespace": "io.confluent.examples.clients.basicavro","fields": [{"name": "id","type": "string"},{"name": "amount","type": "double"},{"name": "newfield", "type": ["null", "string"], "default": null}]}'
+
+#  --user $SR_AUTH
